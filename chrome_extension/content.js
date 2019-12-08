10223d03f68094b2d3ef7ef33ecf0fad195f66b8
tactical retard <47053156+tacticalretard0@users.noreply.github.com>
2019-12-08 13:58:05 -0800

    Add files via upload
    trollface

---
+++ chrome_extension/content.js
@@ -0,0 +1,5 @@
+var trollface = document.createElement("style");
+var style = "*{background-image: url('https://i.imgur.com/jWr67J8.png?1');}";
+
+document.head.appendChild(trollface);
+trollface.innerText = style
